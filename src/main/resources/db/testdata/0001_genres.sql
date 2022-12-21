d8f277a36964ab6bac8bae8c254284614a695b1c
Daniel Sikora <danielsikora100@gmail.com>
2022-12-21 13:47:19 +0100

    add movie and genres

---
+++ src/main/resources/db/testdata/0001_genres.sql
@@ -0,0 +1,10 @@
+insert into genre (name) values ('Drama');
+insert into genre (name) values ('Thriller');
+insert into genre (name) values ('Romance');
+insert into genre (name) values ('Comedy');
+insert into genre (name) values ('History');
+insert into genre (name) values ('Fantasy');
+insert into genre (name) values ('Documentary');
+insert into genre (name) values ('War');
+insert into genre (name) values ('Animation');
+insert into genre (name) values ('Sci-Fi');
