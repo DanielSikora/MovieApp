86135abd11e590d79ae3e57875b76049723edf0c
Adrian Ciochon <34275@student.pwsztar.edu.pl>
2022-12-27 18:48:30 +0100

    404 backend

--- src/main/java/com/movieapp/demo/domain/web/MovieController.java
+++ src/main/java/com/movieapp/demo/domain/web/MovieController.java
@@ -2,10 +2,12 @@ package com.movieapp.demo.domain.web;
 
 import com.movieapp.demo.domain.movie.MovieService;
 import com.movieapp.demo.domain.movie.dto.MovieDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
@@ -19,8 +21,10 @@ public class MovieController {
 
     @GetMapping("/film/{id}")
     public String getMovie(@PathVariable long id, Model model) {
-        Optional<MovieDto> optionalMovie = movieService.findMovieById(id);
-        optionalMovie.ifPresent(movie -> model.addAttribute("movie", movie));
+        MovieDto movie = movieService.findMovieById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        model.addAttribute("movie", movie);
         return "movie";
+
     }
 }
