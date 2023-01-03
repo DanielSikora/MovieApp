c1212c8539381c69bef8e2567fc8be3691553c2b
Daniel Sikora <danielsikora100@gmail.com>
2023-01-03 11:05:53 +0100

    next step add movie

--- src/main/java/com/movieapp/demo/domain/web/admin/AdminController.java
+++ src/main/java/com/movieapp/demo/domain/web/admin/AdminController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 class AdminController {
-
+    public static final String NOTIFICATION_ATTRIBUTE = "notification";
     @GetMapping("/admin")
     public String getAdminPanel() {
         return "admin/admin";
--- src/main/java/com/movieapp/demo/domain/web/admin/MovieManagementController.java
+++ src/main/java/com/movieapp/demo/domain/web/admin/MovieManagementController.java
@@ -1,4 +1,40 @@
 package com.movieapp.demo.domain.web.admin;
 
+import com.movieapp.demo.domain.movie.MovieService;
+import com.movieapp.demo.domain.movie.dto.MovieSaveDto;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
 public class MovieManagementController {
+    private final MovieService movieService;
+
+
+    public MovieManagementController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping("/admin/dodaj-film")
+    public String addMovieForm(Model model) {
+        MovieSaveDto movie = new MovieSaveDto();
+        model.addAttribute("movie", movie);
+        return "admin/movie-form";
+    }
+
+    @PostMapping("/admin/dodaj-film")
+    public String addMovie(MovieSaveDto movie, RedirectAttributes redirectAttributes) {
+        movieService.addMovie(movie);
+        redirectAttributes.addFlashAttribute(
+                AdminController.NOTIFICATION_ATTRIBUTE,
+                "Film %s został zapisany".formatted(movie.getTitle()));
+        return "redirect:/admin";
+    }
 }
