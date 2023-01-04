e0e6fa1c56157462e60f5f0b81b5647fa0a7413f
Krzysiek <krzysiekksi6@gmail.com>
2023-01-04 10:35:50 +0100

    fix after bad push

--- src/main/java/com/movieapp/demo/domain/web/admin/MovieManagementController.java
+++ src/main/java/com/movieapp/demo/domain/web/admin/MovieManagementController.java
@@ -1,5 +1,7 @@
 package com.movieapp.demo.domain.web.admin;
 
+import com.movieapp.demo.domain.genre.GenreService;
+import com.movieapp.demo.domain.genre.dto.GenreDto;
 import com.movieapp.demo.domain.movie.MovieService;
 import com.movieapp.demo.domain.movie.dto.MovieSaveDto;
 import org.springframework.stereotype.Controller;
@@ -16,13 +18,17 @@ import java.util.List;
 @Controller
 public class MovieManagementController {
     private final MovieService movieService;
+    private final GenreService genreService;
 
-    public MovieManagementController(MovieService movieService) {
+    public MovieManagementController(MovieService movieService, GenreService genreService) {
         this.movieService = movieService;
+        this.genreService = genreService;
     }
 
     @GetMapping("/admin/dodaj-film")
     public String addMovieForm(Model model) {
+        List<GenreDto> allGenres = genreService.findAllGenres();
+        model.addAttribute("genres", allGenres);
         MovieSaveDto movie = new MovieSaveDto();
         model.addAttribute("movie", movie);
         return "admin/movie-form";
