a40c90c26544b7384cd07bc60282650c1ec9bec6
Krzysiek <krzysiekksi6@gmail.com>
2023-01-08 21:51:22 +0100

    MovieSaveDto3

--- src/main/java/com/movieapp/demo/domain/genre/dto/GenreDto.java
+++ src/main/java/com/movieapp/demo/domain/genre/dto/GenreDto.java
@@ -1,19 +1,39 @@
 package com.movieapp.demo.domain.genre.dto;
 
+/**
+ * <h1>GenreDto</h1>
+ * The MovieDto program implements an application that represents genre
+ * <p>
+ * <b>Note:</b> Giving proper comments in your program makes it more
+ * user friendly and it is assumed as a high quality code.
+ *
+ * @author  Krzysztof Ksiazek
+ * @author  Daniel Sikora
+ * @author  Adrian Ciochon
+ * @version 1.0
+ * @since   2022-11-04
+ */
 public class GenreDto {
     private Long id;
     private String name;
     private String description;
 
+    /**
+     * Constructor GenreDto Class
+     * @param id id
+     * @param name name
+     * @param description description
+     */
     public GenreDto(Long id, String name, String description) {
         this.id = id;
         this.name = name;
         this.description = description;
     }
 
-    public GenreDto() {
-
-    }
+    /**
+     * Constructor without params
+     */
+    public GenreDto() {}
 
     public Long getId() {
         return id;
