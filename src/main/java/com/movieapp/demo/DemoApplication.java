cc726ae3b6a50a3508c44a715db300598386e285
Krzysiek <krzysiekksi6@gmail.com>
2023-01-08 21:20:22 +0100

    MovieDto javadoc

--- src/main/java/com/movieapp/demo/DemoApplication.java
+++ src/main/java/com/movieapp/demo/DemoApplication.java
@@ -7,15 +7,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @SpringBootConfiguration
 /**
- * Starting class
+ * The DemoApplication program implements starting main application
+ *
+ * @author  Krzysztof Ksiazek
+ * @author  Daniel Sikora
+ * @author  Adrian Ciochon
+ * @version 1.0
+ * @since   2022-11-04
  */
 
+
 public class DemoApplication {
-
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
--- src/main/java/com/movieapp/demo/domain/movie/dto/MovieDto.java
+++ src/main/java/com/movieapp/demo/domain/movie/dto/MovieDto.java
@@ -1,5 +1,8 @@
 package com.movieapp.demo.domain.movie.dto;
 
+/**
+ *
+ */
 public class MovieDto {
     private Long id;
     private String title;
@@ -11,6 +14,20 @@ public class MovieDto {
     private String genre;
     private boolean promoted;
     private String poster;
+
+    /**
+     * Constructor MovieDto Class
+     * @param id id
+     * @param title title
+     * @param originalTitle originalTitle
+     * @param shortDescription shortDescription
+     * @param description description
+     * @param youtubeTrailerId youtubeTrailerId
+     * @param releaseYear releaseYear
+     * @param genre genre
+     * @param promoted promoted
+     * @param poster poster
+     */
     public MovieDto(Long id,
                     String title,
                     String originalTitle,
@@ -33,82 +50,161 @@ public class MovieDto {
         this.poster = poster;
     }
 
+    /**
+     * getPoster method
+     * @return String
+     */
     public String getPoster() {
         return poster;
     }
 
+    /**
+     * setPoster method
+     * @param poster poster
+     */
     public void setPoster(String poster) {
         this.poster = poster;
     }
 
+    /**
+     * getId method
+     * @return Long
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * setId method
+     * @param id id
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * getTitle method
+     * @return String
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * setTitle method
+     * @param title title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * getOriginalTitle method
+     * @return String
+     */
     public String getOriginalTitle() {
         return originalTitle;
     }
 
+    /**
+     * @param originalTitle originalTitle
+     */
     public void setOriginalTitle(String originalTitle) {
         this.originalTitle = originalTitle;
     }
 
+    /**
+     * getShortDescription method
+     * @return String
+     */
     public String getShortDescription() {
         return shortDescription;
     }
 
+    /**
+     * setShortDescription method
+     * @param shortDescription shortDescription
+     */
     public void setShortDescription(String shortDescription) {
         this.shortDescription = shortDescription;
     }
 
+    /**
+     * getDescription method
+     * @return String
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * setDescription method
+     * @param description description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * getYoutubeTrailerId method
+     * @return String
+     */
     public String getYoutubeTrailerId() {
         return youtubeTrailerId;
     }
 
+    /**
+     * setYoutubeTrailerId method
+     * @param youtubeTrailerId youtubeTrailerId
+     */
     public void setYoutubeTrailerId(String youtubeTrailerId) {
         this.youtubeTrailerId = youtubeTrailerId;
     }
 
+    /**
+     * getReleaseYear method
+     * @return Integer
+     */
     public Integer getReleaseYear() {
         return releaseYear;
     }
 
+    /**
+     * setReleaseYear method
+     * @param releaseYear releaseYear
+     */
     public void setReleaseYear(Integer releaseYear) {
         this.releaseYear = releaseYear;
     }
 
+    /**
+     * getGenre method
+     * @return String
+     */
     public String getGenre() {
         return genre;
     }
 
+    /**
+     * setGenre method
+     * @param genre genre
+     */
     public void setGenre(String genre) {
         this.genre = genre;
     }
 
+    /**
+     * isPromoted method
+     * @return boolean
+     */
     public boolean isPromoted() {
         return promoted;
     }
 
+    /**
+     * setPromoted method
+     * @param promoted promoted
+     */
     public void setPromoted(boolean promoted) {
         this.promoted = promoted;
     }
