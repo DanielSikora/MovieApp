1859cc6742ea073093d95b388ac7d0934132aaa0
Adrian Ciochon <34275@student.pwsztar.edu.pl>
2023-01-09 15:41:14 +0100

    JavaDoc Movie

--- src/main/java/com/movieapp/demo/domain/movie/Movie.java
+++ src/main/java/com/movieapp/demo/domain/movie/Movie.java
@@ -5,7 +5,17 @@ import com.movieapp.demo.domain.genre.Genre;
 import javax.persistence.*;
 
 /**
- * Entity class of movie table
+ * <h1>@Entity Movie</h1>
+ * The Movie program implements an application that represents movie
+ * <p>
+ * <b>Note:</b> Giving proper comments in your program makes it more
+ * user friendly and it is assumed as a high quality code.
+ *
+ * @author  Krzysztof Ksiazek
+ * @author  Daniel Sikora
+ * @author  Adrian Ciochon
+ * @version 1.0
+ * @since   2022-11-04
  */
 @Entity
 public class Movie {
@@ -139,26 +149,50 @@ public class Movie {
         this.promoted = promoted;
     }
 
+    /**
+     * getShortDescription method
+     * @return String
+     */
     public String getShortDescription() {
         return shortDescription;
     }
 
+    /**
+     * setShortDescription method
+     * @param shortDescription shortDescription
+     */
     public void setShortDescription(String shortDescription) {
         this.shortDescription = shortDescription;
     }
 
+    /**
+     * getDescription method
+     * @return String
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * setDescription method
+     * @param description v
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * getYoutubeTrailerId method
+     * @return String
+     */
     public String getYoutubeTrailerId() {
         return youtubeTrailerId;
     }
 
+    /**
+     * setYoutubeTrailerId method
+     * @param youtubeTrailerId youtubeTrailerId
+     */
     public void setYoutubeTrailerId(String youtubeTrailerId) {
         this.youtubeTrailerId = youtubeTrailerId;
     }
