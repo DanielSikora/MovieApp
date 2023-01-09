60a6da9a8d39107a535470415bf9d70c225ab7af
Adrian Ciochon <34275@student.pwsztar.edu.pl>
2023-01-09 15:53:38 +0100

    JavaDoc HomeController

--- src/main/java/com/movieapp/demo/domain/web/HomeController.java
+++ src/main/java/com/movieapp/demo/domain/web/HomeController.java
@@ -9,14 +9,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
+/**
+ * <h1>@Controller HomeController</h1>
+ * The program implements an HomeController
+ * <p>
+ * <b>Note:</b> Giving proper comments in your program makes it more
+ * user friendly and it is assumed as a high quality code.
+ *
+ * @author  Krzysztof Ksiazek
+ * @author  Daniel Sikora
+ * @author  Adrian Ciochon
+ * @version 1.0
+ * @since   2022-11-04
+ */
 @Controller
 public class HomeController {
     private final MovieService movieService;
 
+    /**
+     * Constructor HomeController Class
+     * @param movieService movieService
+     */
     public HomeController(MovieService movieService) {
         this.movieService = movieService;
     }
 
+    /**
+     * home "/" method
+     * @param model model
+     * @return
+     */
     @GetMapping("/")
     public String home(Model model) {
         List<MovieDto> promotedMovies = movieService.findAllPromotedMovies();
