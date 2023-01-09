99a6aeca3c936556cc756c90d442ac5d22da60c4
Adrian Ciochon <34275@student.pwsztar.edu.pl>
2023-01-09 15:52:02 +0100

    JavaDoc AdminController

--- src/main/java/com/movieapp/demo/domain/web/GenreController.java
+++ src/main/java/com/movieapp/demo/domain/web/GenreController.java
@@ -13,16 +13,40 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
+/**
+ * <h1>@Controller GenreController</h1>
+ * The program implements an GenreController
+ * <p>
+ * <b>Note:</b> Giving proper comments in your program makes it more
+ * user friendly and it is assumed as a high quality code.
+ *
+ * @author  Krzysztof Ksiazek
+ * @author  Daniel Sikora
+ * @author  Adrian Ciochon
+ * @version 1.0
+ * @since   2022-11-04
+ */
 @Controller
 public class GenreController {
     private final GenreService genreService;
     private final MovieService movieService;
 
+    /**
+     * Constructor GenreController Class
+     * @param genreService genreService
+     * @param movieService movieService
+     */
     public GenreController(GenreService genreService, MovieService movieService) {
         this.genreService = genreService;
         this.movieService = movieService;
     }
 
+    /**
+     * getGenre
+     * @param name name
+     * @param model model
+     * @return String
+     */
     @GetMapping("/gatunek/{name}")
     public String getGenre(@PathVariable String name, Model model) {
         GenreDto genre = genreService.findGenreByName(name)
@@ -36,6 +60,11 @@ public class GenreController {
 
     }
 
+    /**
+     * getGenreList method
+     * @param model model
+     * @return String
+     */
     @GetMapping("/gatunki-filmowe")
     public String getGenreList(Model model) {
         List<GenreDto> genres = genreService.findAllGenres();
