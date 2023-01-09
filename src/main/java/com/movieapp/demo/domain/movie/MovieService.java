6e394948c7b9e115e9d73febaea62bf6ec2b1a9a
Adrian Ciochon <34275@student.pwsztar.edu.pl>
2023-01-09 15:44:24 +0100

    JavaDoc MovieService

--- src/main/java/com/movieapp/demo/domain/movie/MovieService.java
+++ src/main/java/com/movieapp/demo/domain/movie/MovieService.java
@@ -9,15 +9,32 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-
+/**
+ * <h1>@Service MovieService</h1>
+ * The MovieRepository program implements an MovieService Class
+ * <p>
+ * <b>Note:</b> Giving proper comments in your program makes it more
+ * user friendly and it is assumed as a high quality code.
+ *
+ * @author  Krzysztof Ksiazek
+ * @author  Daniel Sikora
+ * @author  Adrian Ciochon
+ * @version 1.0
+ * @since   2022-11-04
+ */
 @Service
 public class MovieService {
-
 
     private final MovieRepository movieRepository;
     private final GenreRepository genreRepository;
     private final FileStorageService fileStorageService;
 
+    /**
+     * Constructor MovieService Class
+     * @param movieRepository movieRepository
+     * @param genreRepository genreRepository
+     * @param fileStorageService fileStorageService
+     */
     public MovieService(MovieRepository movieRepository,
                         GenreRepository genreRepository,
                         FileStorageService fileStorageService) {
@@ -26,22 +43,40 @@ public class MovieService {
         this.fileStorageService = fileStorageService;
     }
 
+    /**
+     * findAllPromotedMovies method
+     * @return List:MovieDto
+     */
     public List<MovieDto> findAllPromotedMovies() {
         return movieRepository.findAllByPromotedIsTrue().stream()
                 .map(MovieDtoMapper::map)
                 .toList();
     }
 
+    /**
+     * findMovieById method
+     * @param id id
+     * @return Optional:MovieDto
+     */
     public Optional<MovieDto> findMovieById(long id) {
         return movieRepository.findById(id).map(MovieDtoMapper::map);
     }
 
+    /**
+     * findMoviesByGenreName method
+     * @param genre genre
+     * @return List:MovieDto
+     */
     public List<MovieDto> findMoviesByGenreName(String genre) {
         return movieRepository.findAllByGenre_NameIgnoreCase(genre).stream()
                 .map(MovieDtoMapper::map)
                 .toList();
     }
 
+    /**
+     * addMovie method
+     * @param movieToSave movieToSave
+     */
     public void addMovie(MovieSaveDto movieToSave) {
         Movie movie = new Movie();
         movie.setTitle(movieToSave.getTitle());
