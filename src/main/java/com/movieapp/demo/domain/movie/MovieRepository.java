427e8c78b1965dd55a578c7b498583503a3f693e
Adrian Ciochon <34275@student.pwsztar.edu.pl>
2023-01-09 15:43:40 +0100

    JavaDoc MovieRepository

--- src/main/java/com/movieapp/demo/domain/movie/MovieRepository.java
+++ src/main/java/com/movieapp/demo/domain/movie/MovieRepository.java
@@ -3,7 +3,30 @@ package com.movieapp.demo.domain.movie;
 import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
+/**
+ * <h1>Interface MovieRepository</h1>
+ * The MovieRepository program implements an interface
+ * <p>
+ * <b>Note:</b> Giving proper comments in your program makes it more
+ * user friendly and it is assumed as a high quality code.
+ *
+ * @author  Krzysztof Ksiazek
+ * @author  Daniel Sikora
+ * @author  Adrian Ciochon
+ * @version 1.0
+ * @since   2022-11-04
+ */
 public interface MovieRepository extends CrudRepository<Movie, Long> {
+    /**
+     * findAllByPromotedIsTrue method
+     * @return List:Movie
+     */
     List<Movie> findAllByPromotedIsTrue();
+
+    /**
+     * findAllByGenre_NameIgnoreCase method
+     * @param genre genre
+     * @return List:Movie
+     */
     List<Movie> findAllByGenre_NameIgnoreCase(String genre);
 }
