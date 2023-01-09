8fbcb7e5ac30966057f2320094878778643abfd7
Adrian Ciochon <34275@student.pwsztar.edu.pl>
2023-01-09 15:46:02 +0100

    JavaDoc AdminController

--- src/main/java/com/movieapp/demo/domain/web/admin/AdminController.java
+++ src/main/java/com/movieapp/demo/domain/web/admin/AdminController.java
@@ -3,9 +3,27 @@ package com.movieapp.demo.domain.web.admin;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * <h1>@Controller AdminController</h1>
+ * The program implements an AdminController
+ * <p>
+ * <b>Note:</b> Giving proper comments in your program makes it more
+ * user friendly and it is assumed as a high quality code.
+ *
+ * @author  Krzysztof Ksiazek
+ * @author  Daniel Sikora
+ * @author  Adrian Ciochon
+ * @version 1.0
+ * @since   2022-11-04
+ */
 @Controller
 class AdminController {
     public static final String NOTIFICATION_ATTRIBUTE = "notification";
+
+    /**
+     * getAdminPanel method
+     * @return String
+     */
     @GetMapping("/admin")
     public String getAdminPanel() {
         return "admin/admin";
