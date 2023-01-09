5e967c47e8db2d4c05b17ffc89eb39de99e085c0
Adrian Ciochon <34275@student.pwsztar.edu.pl>
2023-01-09 15:42:53 +0100

    JavaDoc MovieDtoMapper

--- src/main/java/com/movieapp/demo/domain/movie/MovieDtoMapper.java
+++ src/main/java/com/movieapp/demo/domain/movie/MovieDtoMapper.java
@@ -2,8 +2,25 @@ package com.movieapp.demo.domain.movie;
 
 
 import com.movieapp.demo.domain.movie.dto.MovieDto;
-
+/**
+ * <h1>MovieDtoMapper</h1>
+ * The MovieDtoMapper program implements an application that represents movie
+ * <p>
+ * <b>Note:</b> Giving proper comments in your program makes it more
+ * user friendly and it is assumed as a high quality code.
+ *
+ * @author  Krzysztof Ksiazek
+ * @author  Daniel Sikora
+ * @author  Adrian Ciochon
+ * @version 1.0
+ * @since   2022-11-04
+ */
 class MovieDtoMapper {
+    /**
+     * map method
+     * @param movie movie
+     * @return
+     */
     static MovieDto map(Movie movie) {
         return new MovieDto(
                 movie.getId(),
