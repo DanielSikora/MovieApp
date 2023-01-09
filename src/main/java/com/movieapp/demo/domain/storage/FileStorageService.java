b163479e6f7f8ff5644be6f164116e4a21cc826c
Adrian Ciochon <34275@student.pwsztar.edu.pl>
2023-01-09 15:45:19 +0100

    JavaDoc FileStorageService

--- src/main/java/com/movieapp/demo/domain/storage/FileStorageService.java
+++ src/main/java/com/movieapp/demo/domain/storage/FileStorageService.java
@@ -13,11 +13,29 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+/**
+ * <h1>@Service FileStorageService</h1>
+ * The MovieRepository program implements an MovieService Class
+ * <p>
+ * <b>Note:</b> Giving proper comments in your program makes it more
+ * user friendly and it is assumed as a high quality code.
+ *
+ * @author  Krzysztof Ksiazek
+ * @author  Daniel Sikora
+ * @author  Adrian Ciochon
+ * @version 1.0
+ * @since   2022-11-04
+ */
 @Service
 public class FileStorageService {
     private final String fileStorageLocation;
     private final String imageStorageLocation;
 
+    /**
+     * Constructor FileStorageService service
+     * @param fileStorageLocation fileStorageLocation
+     * @throws FileNotFoundException
+     */
     public FileStorageService(@Value("${app.storage.location}") String fileStorageLocation) throws FileNotFoundException {
         this.fileStorageLocation = fileStorageLocation;
         this.imageStorageLocation = fileStorageLocation + "/img/";
@@ -27,20 +45,41 @@ public class FileStorageService {
         checkDirectoryExists(imageStoragePath);
     }
 
+    /**
+     * checkDirectoryExists method
+     * @param path path
+     * @throws FileNotFoundException
+     */
     private void checkDirectoryExists(Path path) throws FileNotFoundException {
         if (Files.notExists(path)) {
             throw new FileNotFoundException("Directory %s does not exist.".formatted(path.toString()));
         }
     }
 
+    /**
+     * saveImage method
+     * @param file file
+     * @return String
+     */
     public String saveImage(MultipartFile file) {
         return saveFile(file, imageStorageLocation);
     }
 
+    /**
+     * saveFile method
+     * @param file file
+     * @return String
+     */
     public String saveFile(MultipartFile file) {
         return saveFile(file, fileStorageLocation);
     }
 
+    /**
+     * saveFile 2 @param method
+     * @param file file
+     * @param storageLocation storageLocation
+     * @return String
+     */
     private String saveFile(MultipartFile file, String storageLocation) {
         Path filePath = createFilePath(file, storageLocation);
         try {
@@ -51,6 +90,12 @@ public class FileStorageService {
         }
     }
 
+    /**
+     * createFilePath method
+     * @param file file
+     * @param storageLocation storageLocation
+     * @return
+     */
     private Path createFilePath(MultipartFile file, String storageLocation) {
         String originalFileName = file.getOriginalFilename();
         String fileBaseName = FilenameUtils.getBaseName(originalFileName);
